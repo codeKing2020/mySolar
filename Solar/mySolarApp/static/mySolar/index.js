2a5bb3cda35d38a2f896f5ca8e6198f762c59c23
Tavonga <lewistavonga@gmail.com>
2022-06-01 16:12:45 +0200

    User sign-in, register UI complete

--- Solar/mySolarApp/static/mySolar/index.js
+++ Solar/mySolarApp/static/mySolar/index.js
@@ -1,2 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
+    alert("yo!")
+    if (document.title == 'Welcome to MySolar | Home') {
+        alert("removing color!")
+        document.querySelector("nav").classList.remove("bg-white");
+    }
 });
--- Solar/mySolarApp/views.py
+++ Solar/mySolarApp/views.py
@@ -1,5 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponseRedirect, render
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+from .models import User
+from django.db import IntegrityError
 
 # Create your views here.
 def index(request):
     return render(request, "mySolar/index.html")
+
+def login(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        email = request.POST["email"]
+        password = request.POST["password"]
+        user = authenticate(request, username=email, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "mySolar/login.html", {
+                "message": "Invalid email and/or password."
+            })
+    else:
+        return render(request, "mySolar/login.html")
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+def register(request):
+    if request.method == "POST":
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "mySolar/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(email, email, password)
+            user.save()
+        except IntegrityError as e:
+            print(e)
+            return render(request, "mySolar/register.html", {
+                "message": "Email address already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "mySolar/register.html")
